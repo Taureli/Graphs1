f92f913b224d93586b1cba733a19046af27c5dca
Jakub Karolczak <hobkinss@gmail.com>
2015-10-14 13:18:08 +0200

    Minor fixes/cleanup

--- src/com/jakub/grafy/Main.java
+++ src/com/jakub/grafy/Main.java
@@ -8,30 +8,35 @@ public class Main {
 	static int matrixLength = 0;
 	
 	public static void main(String args[]) throws Exception{
-		int command;
-		System.out.println("1 - za³aduj macierz z pliku;");
-		System.out.println("Wybierz polecenie: ");
+		int command = -1;
 		Scanner scan = new Scanner(System.in);
-		command = scan.nextInt();
-		scan.close();
-		
-		if(command == 1){
-			loadMatrix();
-			
-			System.out.println("Wczytano macierz z pliku: ");
-			for(int i = 0; i < matrixLength; i++){
-				System.out.println();
-				for(int j = 0; j < matrixLength; j++){
-					System.out.print(matrix[i][j] + " | ");
+		while(command != 0){
+			System.out.println("\n0 - zakoñcz program;");
+			System.out.println("1 - za³aduj macierz z pliku;");
+			System.out.println("Wybierz polecenie: ");
+			command = scan.nextInt();
+
+			if(command == 1){
+				loadMatrix();
+				
+				System.out.println("Wczytano macierz z pliku: ");
+				for(int i = 0; i < matrixLength; i++){
+					System.out.println();
+					for(int j = 0; j < matrixLength; j++){
+						System.out.print(matrix[i][j] + " | ");
+					}
 				}
 			}
 		}
+		scan.close();
 		
 	}
 
+	//Wczytywanie macierzy z pliku
 	public static void loadMatrix() throws Exception {
 		File file = new File("Macierz.txt");
 		Scanner sc = new Scanner(file);
+		matrixLength = 0;
 
 		//Sprawdzam wielkoœæ macierzy
 		String[] line = sc.nextLine().trim().split(" ");
